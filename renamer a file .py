594f1d6fdb41280ee3046f202f97b5dc157d101a
Tanish Gupta <tanishgupta12389@gmail.com>
2024-12-15 09:33:17 +0530

    updated

--- renamer a file .py
+++ renamer a file .py
@@ -5,3 +5,10 @@ import os
 curdir=os.getcwd()
 print(curdir)
 
+# change th name of file by path 
+os.rename('D:\\vscode\\py.work\\asset\\osfile.txt', 'D:\\vscode\\py.work\\asset\\osnewfile.txt')
+#use '\\' to avoid issues with escape sequences
+
+# OR >>  os.rename(r'D:\vscode\py.work\asset\osfile.txt', r'D:\vscode\py.work\asset\osnewfile.txt')
+#use 'r' before string to avoid issues with escape sequences
+print('renamew sucessfully')
