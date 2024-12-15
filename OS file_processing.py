70afad4fa31be526652e6c0f8c5f980925f658f4
Tanish Gupta <tanishgupta12389@gmail.com>
2024-12-15 09:43:30 +0530

    os file updated

--- OS file_processing.py
+++ OS file_processing.py
@@ -4,6 +4,9 @@ import os
 #use to print current working directory 
 curdir=os.getcwd()
 print(curdir)
+# use to change current working directory
+# os.chdir('/home/your_username/your_directory_name')
+# use path of the directory you want to change to
 
 # change th name of file by path 
 os.rename('D:\\vscode\\py.work\\asset\\osfile.txt', 'D:\\vscode\\py.work\\asset\\osnewfile.txt')
@@ -11,4 +14,9 @@ os.rename('D:\\vscode\\py.work\\asset\\osfile.txt', 'D:\\vscode\\py.work\\asset\
 
 # OR >>  os.rename(r'D:\vscode\py.work\asset\osfile.txt', r'D:\vscode\py.work\asset\osnewfile.txt')
 #use 'r' before string to avoid issues with escape sequences
-print('renamew sucessfully')
+print('renamed sucessfully')
+
+# to delete a file
+os.remove('D:\\vscode\\py.work\\asset\\delos.txt')
+print("Deleted sucessfully")
+
