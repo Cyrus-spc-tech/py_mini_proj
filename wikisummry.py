656e7205ad0224dd5d193dbd18dc2480900351b0
Tanish Gupta <tanishgupta12389@gmail.com>
2024-12-28 13:16:23 +0530

    valid input lines

--- wikisummry.py
+++ wikisummry.py
@@ -6,6 +6,16 @@ print('Lets Generate Wiki Summary!\n')
 
 # Get user input for search term
 src = input("What You want to search About: ")
-line = int(input("How many lines you want to read: "))
+# Get user input for number of lines and validate it
+while True:
+    try:
+        line = int(input("How many lines you want to display: "))
+        if line > 0:
+            break
+        else:
+            print("Please enter a positive integer.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        
 kt.info(src,lines=line)
 #kt.search(src)
