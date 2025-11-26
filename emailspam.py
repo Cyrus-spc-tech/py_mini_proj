4e2848af104f1da850d1ae5144bdc301c702f375
Tanish Gupta <tanishgupta12389@gmail.com>
2025-11-26 13:03:34 +0530

    mailboomber

--- emailspam.py
+++ emailspam.py
@@ -7,11 +7,9 @@ ob.login("testemmail2024@tempmail.com","G7#x9!pVq2&zL8rT")
 subject="Testing Scam"
 body="SELECT employee_id FROM EmployeesWHERE salary < 30000 AND manager_id IS NOT NULL AND manager_id IN (SELECT employee_id FROM Employees);"
 
-while True:
-    massage="Subject:{}\n\n{}".format(subject,body)
-    listadd=['sendermail@gmail.com']
-    ob.sendmail('testemmail2024@tempmail.com',listadd,massage)
-    print("sended")
-    ans=input("Do you want to send again ? (yes/no): ")
-    if ans.lower()!='yes':
-        break
+n = int(input("Enter no of time to bombing : "))
+for i in range(n):
+    message = "Subject:{}\n\n{}".format(subject, body)
+    recipients = ['recipient@example.com']
+    ob.sendmail('testemmail2024@tempmail.com', recipients, message)
+    print("Message sent")
